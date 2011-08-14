b15fed50236b970cce48b3a2d0d21f7355d85d7e
James Colley <me@jcolley.co.uk>
2011-08-14 21:53:54 +0100

    Revert "Up to dare"
    This reverts commit 0b554fd5fa825e83038648ecfd7f40821a0625e5.

--- Install files/ccpusher.sh
+++ Install files/ccpusher.sh
@@ -2,4 +2,4 @@
 # sudo dhclient -r
 # sudo dhclient
 cd /home/jzc
-sudo java -jar ccpusher_direct.jar 66 /dev/ttyUSB0 me@jcolley.co.uk 9806bf87d33c3063a055b4f4224944a5
+sudo java -jar ccpusher_direct.jar 54 /dev/ttyUSB0 me@jcolley.co.uk 9806bf87d33c3063a055b4f4224944a5
